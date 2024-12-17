6c9ed0fcdc35f2ee90829c5e18607e5897f3cf43
mustafaturk <elturkmustafa@gmail.com>
2024-12-17 09:26:39 +0100

    firt commit

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping
+    public String greeting() {
+        return "Greetings from Spring Boot!";
+    }
+}
+
